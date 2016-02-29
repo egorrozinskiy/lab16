2f6a3b023f7e72b7c7f23857c6e3c9394f1e6b47
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-02-29 11:29:31 +0300

    Update upr1.py

--- upr1.py
+++ upr1.py
@@ -1,44 +1,4 @@
-import argparse
 import sys
-parser = argparse.ArgumentParser(
-    description='Calc'
-)
-parser.add_argument(
-    'values',
-    metavar='VALUES',
-    type=float,
-    nargs=2,
-    help='data'
-)
-parser.add_argument(
-    '-a',
-    '--action',
-    metavar='SIGN',
-    help='sign'
-)
-parser.add_argument(
-    '-v',
-    '--verbose',
-    action='store_true',
-    help='result'
-)
-args = parser.parse_args()
-print(args.action)
-if not args.action[0] in  ['+', '-', '*', '/']:
-    print(
-        'incor sign'
-    )
-    sys.exit(-1)
-
-   
-if args.action[0] == '+':
-    result = float(args.values[0]) + float(args.values[1])
-elif args.action[0] == '-':
-    result = float(args.values[0]) - float(args.values[1])
-elif args.action[0] == '*':
-    result = float(args.values[0]) * float(args.values[1])
-elif args.action[0] == '/':
-    result = float(args.values[0]) / float(args.values[1])
-
-if args.verbose:
-    print('Result', result)
+for arg in sys.argv[1:]:
+	if len(arg)%3==0:
+		print(arg)
